6ad061b50e6ba5b1ea44fadaedb8be023a220ce8
Manikandan9128 <104370904+Manikandan9128@users.noreply.github.com>
2023-02-24 20:38:08 +0530

    deletevisit

---
+++ deletevisited.php
@@ -0,0 +1,10 @@
+<?php
+include("connect.php");
+if(isset($_GET["visitedid"])){
+    $id= $_GET["visitedid"];
+    $sql="delete from visited where id=$id";
+    $result=mysqli_query($conn,$sql);
+    if($result){
+        header("location:visited.php");
+    }
+}
